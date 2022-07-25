fc2ca8a12c62309554e7c3ed9892f451d028aa11
Domingo Gallardo <domingo.gallardo@ua.es>
2022-07-25 11:44:55 +0200

    Más ejemplos

--- posts/0004-result-builders-y-3/code/result-builders-3.swift
+++ posts/0004-result-builders-y-3/code/result-builders-3.swift
@@ -34,18 +34,54 @@ func holaMundo() -> String {
 func holaMundoTransformada() -> String {
     let v0 = "Hola"
     let v1 = "mundo"
-    let v2 = Bool.random() ?
-                StringConcatenator.buildEither(first: "cruel") :
-                StringConcatenator.buildEither(second: "divertido")
-    let v3 = StringConcatenator.buildBlock(v0, v1, v2)
-    var v4: [String] = []
+    var v3: String? = nil
+    if Bool.random() {
+        let v3a = "cruel"
+        v3 =  StringConcatenator.buildEither(first: v3a)
+    } else {
+        let v3b = "divertido"
+        v3 = StringConcatenator.buildEither(second: v3b)
+    }
+    let v4 = StringConcatenator.buildBlock(v0, v1, v3!)
+    var v5: [String] = []
     for i in (0...10) {
         let vi = "\(i)"
-        v4.append(vi)
+        v5.append(vi)
     }
-    let v5 = StringConcatenator.buildArray(v4)
-    return StringConcatenator.buildBlock(v3, v5)
+    let v6 = StringConcatenator.buildArray(v5)
+    return StringConcatenator.buildBlock(v4, v6)
 }
 
 print(holaMundo())
 print(holaMundoTransformada())
+
+@resultBuilder
+struct ArrayBuilder {
+    static func buildExpression(_ expression: Int) -> [Int] {
+        return [expression]
+    }
+
+    static func buildBlock(_ components: [Int]...) -> [Int] {
+        return Array(components.joined())
+    }
+
+    static func buildFinalResult(_ component: [Int]) -> [Double] {
+        component.map {Double($0)}
+    }
+
+    static func buildArray(_ components: [[Int]]) -> [Int] {
+        return Array(components.joined())
+    }
+}
+
+@ArrayBuilder
+func buildArray() -> [Double] {
+    100
+    100+100
+    (100+100)*2
+    for i in (1...3) {
+        i
+    }
+}
+
+print(buildArray())
